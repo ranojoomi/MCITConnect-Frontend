3bf66c5686e42014e8f51897a7e8e3f6b13b254b
Radin Nojoomi <ranojoomi@gmail.com>
2023-02-19 15:10:39 -0500

    add assets to background

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {Text} from 'react-native';
+import BLeft from '../assets/Bottom_Left.png'
+import BRight from '../assets/Bottom_Right.png'
+import TRight from '../assets/Top_Right.png'
 
 
 
@@ -11,9 +14,13 @@ const HomePage = () => {
     return (
         <div className="homepage App">
             <h2 className='Title'>CIT CONNECT</h2>
+            <h3 subTitle="sub">Fun for MCIT</h3>
             <Text numberOfLines={1}>
                 <p>Click to begin your {link}</p>
             </Text>
+            <img className='BLeft' src={BLeft} alt="Beautiful" />
+            <img className='BRight' src={BRight} alt="Rock" /> 
+            <img className='TRight' src={TRight} alt="Rocks" />
         </div>
     );
 }
