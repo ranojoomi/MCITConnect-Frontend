1b318bfa6a2ebcc01f3dded426c04d0be989988e
Radin Nojoomi <ranojoomi@gmail.com>
2023-02-26 11:48:20 -0500

    add some changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,7 @@ import {Text} from 'react-native';
 import BLeft from '../assets/Bottom_Left.png'
 import BRight from '../assets/Bottom_Right.png'
 import TRight from '../assets/Top_Right.png'
+import text_bcg from '../assets/test_bcg.png'
 
 
 
@@ -14,13 +15,14 @@ const HomePage = () => {
     return (
         <div className="homepage App">
             <h2 className='Title'>CIT CONNECT</h2>
-            <h3 subTitle="sub">Fun for MCIT</h3>
+            <h3 className="sub">Fun for MCIT</h3>
             <Text numberOfLines={1}>
-                <p>Click to begin your {link}</p>
+                <p className='mainLink'>Click to begin your&nbsp;{link}</p>
             </Text>
             <img className='BLeft' src={BLeft} alt="Beautiful" />
             <img className='BRight' src={BRight} alt="Rock" /> 
             <img className='TRight' src={TRight} alt="Rocks" />
+            <img className='text_bcg' src={text_bcg} alt="Should be bcg" />
         </div>
     );
 }
