2ae4b71c290a02f3c115de4329052bda7589d516
jakethe555 <jake5@naver.com>
2023-02-26 19:07:30 -0500

    Edit beta Match page and add NameForm component

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,16 +1,69 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import NameForm from "./NameForm";
 
-const Match = () => {
+class Match extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { firstName: "", lastName: "", email: "" };
 
-    const link = <Link to="/homepage"></Link>
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    return (
-        <div className="Contents App">
-            <h1>This is a work in progress</h1>
-            <button onClick={link}>Go back</button>
-        </div>
-    );
+    // handleChange(event) {
+    //     this.setState({ value: event.target.value });
+    // }
+
+    handleSubmit(event) {
+        alert(
+            "First Name: " +
+                this.state.firstName +
+                " Last Name: " +
+                this.state.lastName +
+                " Email: " +
+                this.state.email
+        );
+        event.preventDefault();
+    }
+
+    changeFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value,
+        });
+    };
+
+    changeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value,
+        });
+    };
+
+    changeEmail = (event) => {
+        this.setState({
+            email: event.target.value,
+        });
+    };
+
+    render() {
+        return (
+            <div className="homepage App">
+                <h2 className="Title">CIT CONNECT</h2>
+                <div className="Content">
+                    <NameForm
+                        firstName={this.state.firstName}
+                        handleFirstNameChange={this.changeFirstName}
+                        lastName={this.state.lastName}
+                        handleLastNameChange={this.changeLastName}
+                        email={this.state.email}
+                        handleEmailChange={this.changeEmail}
+                    ></NameForm>
+
+                    <button onClick={this.handleSubmit}>text</button>
+                </div>
+            </div>
+        );
+    }
 }
- 
+
 export default Match;
